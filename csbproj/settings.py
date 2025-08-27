5bfdc7e2455d766aae6bd22407d8b4f31758ce42
lsommarberg <laura.sommarberg@gmail.com>
2025-08-27 11:03:21 +0300

    fix

--- csbproj/settings.py
+++ csbproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i816klc9dn=298i8+7i24aqpn$#h6z-(vxywim3h7a!bt$6s5+"
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fallback-key-for-development')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
